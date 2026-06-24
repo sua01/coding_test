1298e33fb4a76feac36aa36037312a842618c25b
sua <65943338+sua01@users.noreply.github.com>
2026-06-24 22:44:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.28 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 54.5 MB, 시간: 6.17 ms
+메모리: 58.1 MB, 시간: 5.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 15일 23:31:21
+2026년 06월 24일 22:44:45
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int[][] dist;
-    static int N, M;
-    static int[][] maps;
+    static int[] dx = {-1, 1, 0,0};
+    static int[] dy = {0, 0, -1,1};
     
-    // 0:벽, 1:이동가능
     public int solution(int[][] maps) {
-        int answer = 0;
-        this.maps = maps;
-        N = maps.length;
-        M = maps[0].length;
-        bfs();
-        answer = dist[N-1][M-1];
-        if(answer==0) answer = -1;
-        return answer;
-    }
-    
-    // visited 대신 dist 사용해서 최단거리 구하기
-    public static void bfs(){
-        dist = new int[N][M];
+        int answer = -1;
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        int[][] dist = new int[n][m];   // 최단거리 저장
         ArrayDeque<int[]> q = new ArrayDeque<>();
+        
+        // bfs
         q.offer(new int[] {0,0});
         dist[0][0] = 1;
-        
         while(!q.isEmpty()){
-            int[] curr = q.poll();
+            int[] cur = q.poll();
+            
+            // 최단거리 리턴
+            if(cur[0] == n-1 && cur[1] == m-1) return  dist[cur[0]][cur[1]];
             
             for(int i=0; i<4; i++){
-                int nx = curr[0] + dx[i];
-                int ny = curr[1] + dy[i];
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
                 
-                if(nx <0||ny <0 || nx >=N || ny >= M) continue;    // map 범위 밖
-                if(maps[nx][ny] == 0) continue;    // 벽인 경우
-                if(dist[nx][ny] != 0) continue; // 이미 방문한 경우
+                if(nx < 0 || nx > n-1 || ny <0 || ny >m-1) continue;
+                if(maps[nx][ny] == 0) continue;
+                if(dist[nx][ny] != 0) continue;
                 
+                dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
                 q.offer(new int[] {nx, ny});
-                dist[nx][ny] = dist[curr[0]][curr[1]] + 1;
             }
         }
         
         
+        return answer;
     }
 }
