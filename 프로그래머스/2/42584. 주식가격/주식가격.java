1cd7976f6137e623903f13573a0bbbd60cb3033b
sua <65943338+sua01@users.noreply.github.com>
2026-06-25 14:01:30 +0900

    [level 2] Title: 주식가격, Time: 22.18 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,20 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int[] answer = {};
         
-        for(int i=0; i<prices.length-1; i++){
-            int cnt = 0;
-            int j = i+1;  // 비교할 원소
-            while(j<=prices.length-1){
-                cnt++;
-                if(prices[i] > prices[j])
-                    break;
-                j++;
+        Stack<Integer> s = new Stack<>();
+        int n = prices.length;
+        answer = new int[n];
+        s.push(0);
+        
+        for(int i=1; i<n; i++){
+            while(!s.isEmpty() && prices[s.peek()] > prices[i]){
+                // 가격 떨어진 경우
+                int p = s.pop();
+                answer[p] = i - p;
             }
-            answer[i] = cnt;
+            s.push(i);
         }
-        answer[prices.length-1] = 0;
+        
+        while(!s.isEmpty()){
+            int p = s.pop();
+            answer[p] = n - 1 - p;
+        }
+        
         return answer;
     }
 }
