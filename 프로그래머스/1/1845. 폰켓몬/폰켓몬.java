cf60a4d71617685037f413a4bacad363005016b6
sua <65943338+sua01@users.noreply.github.com>
2026-06-25 10:46:15 +0900

    [level 1] Title: 폰켓몬, Time: 2.65 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,23 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Map<Integer, Integer> pocket = new HashMap<>();
+        int answer = 0; // 가장 많은 종류의 폰켓몬 선택했을 때의 종류 번호 개수
         
-        // 폰켓몬 종류 별로 마리 저장
-        for(int num: nums){
-            pocket.put(num, pocket.getOrDefault(num, 0) + 1);
+        HashMap<Integer, Integer> p = new HashMap<>();
+        for(int n : nums){
+            p.put(n, p.getOrDefault(n, 0)+1);
         }
         
-        int n = nums.length;
-        if(n/2 < pocket.size()){
-            answer = n/2;
-        }
-        else{
-            answer = pocket.size();
-        }
-        
-        // 가장 많은 종류의 폰켓몬 선택
+        int m = nums.length/2;
+        if(p.size() < m) answer = p.size();
+        else answer = m;
         return answer;
     }
 }
