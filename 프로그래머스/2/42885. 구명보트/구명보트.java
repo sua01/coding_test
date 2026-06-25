ac61faf502e1c640efc029cbfcdbd13a5b6ebd72
sua <65943338+sua01@users.noreply.github.com>
2026-06-25 15:06:11 +0900

    [level 2] Title: 구명보트, Time: 10.42 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,19 +4,23 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         
-        // 몸무게 오름차순 정렬
+        // 정렬
         Arrays.sort(people);
-        int left = 0;
-        int right = people.length-1;
         
+        // 투포인터 이용
+        int left = 0;
+        int right = people.length - 1;
+        
+        // 무거운 사람부터 태우기
         while(left <= right){
-            if(people[left] + people[right] <= limit){
+            if(limit - people[right] >= people[left]){
+                // 더 태울 수 있는 경우
                 left++;
             }
+            // 무거운 사람만 탈 수 있는 경우
             right--;
             answer++;
         }
-        
         return answer;
     }
 }
