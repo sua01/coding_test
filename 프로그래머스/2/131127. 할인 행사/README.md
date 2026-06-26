0aabf31622147e7f54e95a935c40275bd5dbbdf3
sua <65943338+sua01@users.noreply.github.com>
2026-06-26 14:54:13 +0900

    [level 2] Title: 할인 행사, Time: 24.87 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 46.23 ms
+메모리: 107 MB, 시간: 24.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 28일 23:21:57
+2026년 06월 26일 14:54:11
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,57 +1,38 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        // 원하는 제품, 개수 저장
-        Map<String, Integer> need = new HashMap<>();
-        for(int i=0; i<want.length; i++){
-            need.put(want[i], number[i]);
+        int n = 0;  // 총 원하는 제품 수량
+        for(int num : number){
+            n+= num;
         }
         
-        // 할인 기간이 10일 미만인 경우 > X
-        if(discount.length < 10){
-            return 0;
+        // 원하는 물품, 개수 해시맵 저장
+        HashMap<String, Integer> w = new HashMap<>();
+        for(int i=0; i<want.length;i++){
+            w.put(want[i], number[i]);
         }
         
-        // 슬라이딩 윈도우
-        Map<String, Integer> window = new HashMap<>();
-        for(int i=0; i<10; i++){
-            window.put(discount[i], window.getOrDefault(discount[i], 0)+1);
+        // 윈도우 슬라이딩으로 비교
+        // 먼저 n개 넣어두기
+        HashMap<String, Integer> s = new HashMap<>();
+        for(int i=0; i<n; i++){
+            s.put(discount[i], s.getOrDefault(discount[i], 0)+1);
         }
         
-        // 첫번째 10일 구간 체크하기
-        if(isValid(need, window)){
-            answer++;
-        }
-        
-        // 10일씩 체크하기 : 하나 빼고 하나 추가하기
-        for(int i=10; i<discount.length; i++){
-            String out = discount[i-10];
-            String in = discount[i];
+        for(int i=0; i<=discount.length-n; i++){
+            // 비교
+            if (w.equals(s)) answer++;
             
-            // out 제거
-            int outCnt = window.getOrDefault(out, 0) - 1;
-            if(outCnt <= 0) window.remove(out);
-            else window.put(out, outCnt);
+            // 슬라이딩 밀기
+            if(i == discount.length - n) break; // 끝이라서 안밀어도 되야하는 경우
+            s.put(discount[i], s.getOrDefault(discount[i], 0)-1);
+            if(s.getOrDefault(discount[i], 0) == 0) s.remove(discount[i]);
+            s.put(discount[i+n], s.getOrDefault(discount[i+n], 0)+1);
             
-            // in 추가
-            window.put(in, window.getOrDefault(in, 0)+1);
-            
-            if(isValid(need, window)) answer++;
         }
+        
         return answer;
-    }
-    
-    // 원하는 제품이랑 10일 연속 할인 제품이 일치하는지 확인
-    public boolean isValid(Map<String, Integer> need, Map<String, Integer> window){
-        for(Map.Entry<String, Integer> e: need.entrySet()){
-            String item = e.getKey();
-            int cnt = e.getValue();
-            
-            if(window.getOrDefault(item, 0) != cnt) return false;
-        }
-        return true;
     }
 }
