ac94410361d9f7f0a092d680ca21ddb75b6a32aa
sua <65943338+sua01@users.noreply.github.com>
2026-06-26 15:18:10 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,21 +1,19 @@
 import java.util.*;
 
 class Solution {
-    // 서로 다른 옷의 조합의 수
     public int solution(String[][] clothes) {
+        HashMap<String, Integer> c = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            c.put(clothes[i][1], c.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
         int answer = 1;
-        
-        // 해시맵 > 종류별로 개수 저장하기
-        HashMap<String, Integer> cnt = new HashMap<>();
-        for(String[] c : clothes){
-            cnt.put(c[1], cnt.getOrDefault(c[1], 0)+1);
+
+        for (int count : c.values()) {
+            answer *= (count + 1);
         }
-        
-        // 조합의 수 구하기
-        for(int v : cnt.values()){
-            answer*= (v+1);
-        }
-        
-        return answer-1;    // 모두 안 입는 경우의 수 제외
+
+        return answer - 1;
     }
 }
